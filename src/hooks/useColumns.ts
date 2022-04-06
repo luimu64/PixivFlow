a5f9b52a070f1cb9550a40e56084d0ca0faf592f
luimu64 <leimuraittinen@gmail.com>
2022-04-06 22:00:46 +0300

    column resizing

--- src/hooks/useColumns.ts
+++ src/hooks/useColumns.ts
@@ -3,9 +3,10 @@ import { useImages } from "./useImages";
 
 export const useColumns = (columnWrapper: any) => {
     const [columns, setColumns] = useState<string[][]>([]);
+    const [resizing, setResizing] = useState<boolean>(false);
     const { getImages } = useImages();
 
-    const getShortestColumn = () => {
+    const getShortestColumn = async () => {
         const heights = Array.from((columnWrapper.current as HTMLElement).children).map(e => e.clientHeight);
         const min = Math.min(...heights);
         return heights.indexOf(min);
@@ -13,23 +14,35 @@ export const useColumns = (columnWrapper: any) => {
 
     const loadNewImage = async () => {
         if (columnWrapper.current) {
-            const newImg = await getImages(1);
-            const shortest = getShortestColumn();
+            const newImg = await getImages(1) || [];
+            const shortest = await getShortestColumn();
             const tempColumn = [...columns];
             tempColumn[shortest].push(newImg[0]);
             setColumns(tempColumn)
         }
     }
 
-    useEffect(() => {
-        const getInitialData = async () => {
-            let tempImgs: string[][] = Array(Math.ceil(window.innerWidth / 600)).fill(undefined).map(() => []);
-            let images = await getImages(20);
-            images.forEach((e, i) => tempImgs[i % tempImgs.length].push(e));
-            setColumns(tempImgs);
-        }
+    const initializeColumns = async (images: string[]) => {
+        let tempImgs: string[][] = Array(Math.ceil(window.innerWidth / 600)).fill(undefined).map(() => []);
+        images.forEach((e, i) => tempImgs[i % tempImgs.length].push(e));
+        setColumns(tempImgs);
+    }
 
-        getInitialData();
+    useEffect(() => {
+        if (resizing) {
+            setTimeout(() => {
+                initializeColumns(columns.flat());
+                setResizing(false);
+            }, 1000)
+        }
+    }, [resizing])
+
+    useEffect(() => {
+        (async () => initializeColumns(await getImages(20)))();
+
+        window.addEventListener('resize', () => setResizing(true));
+
+        return () => window.removeEventListener('resize', () => setResizing(true));
     }, []);
 
     return { columns, loadNewImage };
